4656f1682863f1c4ae2dae67695e521ad33048eb
reinita18 <reynavillegas1818@hotmail.com>
2023-12-19 21:06:41 -0700

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReportComponent } from './components/report/report.component';
+import { ReportListComponent } from './components/report-list/report-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ReportComponent,
+    ReportListComponent
   ],
   imports: [
     BrowserModule,
